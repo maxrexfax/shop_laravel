e4a99c31e23f3bc44240b06661e57bad567a0e38
maxrexfax <maxymbarannyk@gmail.com>
2021-01-15 09:35:44 +0200

    Sort order for images 002 commit

--- resources/views/categories/products.blade.php
+++ resources/views/categories/products.blade.php
@@ -5,6 +5,23 @@
                 <div class="card col-md-12 p-0">
                     @include('menu._category_menu')
                 </div>
+            <div class="d-flex flex-wrap justify-content-space">
+                @foreach($products as $productCategory)
+                    <div class="col-lg-2 col-md-3 col-sm-12 border rounded m-2">
+                        <a class="url_no_decoration" href="{{route ('product.show', ['id' => $productCategory->product->id] )}}">
+                            <img width="100%" src="{{asset('/img/logo/' . $productCategory->product->logo_image)}}">
+                        </a>
+                    <h2>
+                        <a class="url_no_decoration" href="{{route ('product.show', ['id' => $productCategory->product->id] )}}">
+                            {{$productCategory->product->product_name}}
+                        </a>
+                    </h2>
+                        <p style="overflow: hidden;">{{$productCategory->product->short_description}}</p>
+                        <p>Price: <b>{{$productCategory->product->price}}</b>$</p>
+                        <button class="btn btn-secondary w-100 m-0">Add to cart</button>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/menu/_category_blocks.blade.php
+++ resources/views/menu/_category_blocks.blade.php
@@ -3,7 +3,14 @@
         @foreach($categories as $category)
         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-10 col-12 border m-2 text-center">
 
-                <img height="100px" src="{{ asset('/img/empty.png') }}" alt="No logo"/>
+
+            @if($category->category_logo)
+                <img height="150px" src="{{ asset('img/logo/' . $category->category_logo) }}" alt="{{$category->category_name}}"/>
+            @else
+                <img height="150px" src="{{ asset('img/empty.png') }}" alt="No logo"/>
+            @endif
+
+
                 <h3>
                     <a class="url_no_decoration" href="{{route ('product.category', ['id' => $category->id] )}}">
                         {{$category->category_name}}

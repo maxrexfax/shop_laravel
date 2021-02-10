9140a8f7198e7c7200eebb114e3b77bf9d004505
maxrexfax <maxymbarannyk@gmail.com>
2021-02-10 12:59:49 +0200

    Microfix commit

--- app/Image.php
+++ app/Image.php
@@ -7,7 +7,7 @@ use App\Product;
 
 class Image extends Model
 {
-    public const PATH_TO_SAVE_PRODUCT_IMAGES = '/img/images';
+    public const PATH_TO_SAVE_PRODUCT_IMAGES = '/img/images/';
     public const PATH_TO_SAVE_LOGOS = '/img/logo/';
     protected $fillable = [
         'image_name',

e7ad6fb73275e1c25a00688fee50e2da134021ce
maxrexfax <maxymbarannyk@gmail.com>
2021-02-18 11:59:34 +0200

    Cart for desktop and mobile commit

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -2,63 +2,100 @@
 @section('content')
     <div class="w-100 bg-white p-0">
         <section class="place-holder"></section>
-        <div class="container shopping-cart-main">
+        <div class="shopping-cart-main">
             <form method="POST" action="{{ route('cart.calculate') }}">
             <div class="row">
                     @csrf
                 <div class="offset-md-1 col-md-7 col-sm-12">
                     <h2 class="h4">{{__('messages.shopping_cart')}}</h2>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th colspan="2">{{__('messages.product_details')}}</th>
-                                <th>{{__('messages.quantity')}}</th>
-                                <th>{{__('messages.price')}}</th>
-                                <th>{{__('messages.total')}}</th>
-                                <th>{{__('messages.delete')}}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-
+                    <div class="d-none d-md-block overflow-auto">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th colspan="2">{{__('messages.product_details')}}</th>
+                                    <th>{{__('messages.quantity')}}</th>
+                                    <th>{{__('messages.price')}}</th>
+                                    <th>{{__('messages.total')}}</th>
+                                    <th>{{__('messages.delete')}}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @if(isset($cart->product_rows))
+                            @foreach($cart->product_rows as $product)
+                                <tr>
+                                    <td>
+                                        <div class="image-in-cart">
+                                            <a href="{{route('product.show', ['id' => $product['product_id']])}}" target="_blank">
+                                                <img class="w-100" src="{{asset('/img/logo/' . $product['product_logo'])}}">
+                                            </a>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <h3>{{$product['product_name']}}</h3>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <input type="hidden" value="{{$product['product_id']}}" name="product_ids[]">
+                                        <input type="hidden" value="{{$product['product_price']}}" name="product_prices[]">
+                                        <input type="number" min="0" max="999" id="{{$product['product_id']}}" name="quantity[]" class="text-center pl-3 input-product-quantity-cart form-control" value="{{$product['product_quantity']}}">
+                                    </td>
+                                    <td>
+                                       {{$cart->calculatePrice($product['product_price'])}}{{$cart->getCurrencySymbol()}}
+                                    </td>
+                                    <td class="row-price-holder">
+                                        {{$cart->calculatePrice($product['product_row_price'])}}{{$cart->getCurrencySymbol()}}
+                                    </td>
+                                    <td>
+                                        <span class="font-italic">
+                                            <a data-id="{{$product['product_id']}}" data-confirm="{{__('actions.really_delete?')}}" class="delete-from-cart" href="{{route('cart.delete', ['id' => $product['product_id']])}}" title="{{__('messages.remove_this_item_from_cart')}}">
+                                                <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
+                                            </a>
+                                        </span>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            @endif
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="d-md-none d-lg-none">
                         @if(isset($cart->product_rows))
-                        @foreach($cart->product_rows as $product)
-                            <tr>
-                                <td>
-                                    <div class="image-in-cart">
-                                        <a href="{{route('product.show', ['id' => $product['product_id']])}}" target="_blank">
-                                            <img class="w-100" src="{{asset('/img/logo/' . $product['product_logo'])}}">
-                                        </a>
-                                    </div>
-                                </td>
-                                <td>
+                            @foreach($cart->product_rows as $product)
+                                <div>
                                     <div>
                                         <h3>{{$product['product_name']}}</h3>
                                     </div>
-                                </td>
-                                <td>
-                                    <input type="hidden" value="{{$product['product_id']}}" name="product_ids[]">
-                                    <input type="hidden" value="{{$product['product_price']}}" name="product_prices[]">
-                                    <input type="number" min="0" max="999" id="{{$product['product_id']}}" name="quantity[]" class="text-center pl-3 input-product-quantity-cart form-control" value="{{$product['product_quantity']}}">
-                                </td>
-                                <td>
-                                   {{$cart->calculatePrice($product['product_price'])}}{{$cart->getCurrencySymbol()}}
-                                </td>
-                                <td class="row-price-holder">
-                                    {{$cart->calculatePrice($product['product_row_price'])}}{{$cart->getCurrencySymbol()}}
-                                </td>
-                                <td>
-                                    <span class="font-italic">
-                                        <a data-id="{{$product['product_id']}}" data-confirm="{{__('actions.really_delete?')}}" class="delete-from-cart" href="{{route('cart.delete', ['id' => $product['product_id']])}}" title="{{__('messages.remove_this_item_from_cart')}}">
-                                            <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
-                                        </a>
-                                    </span>
-                                </td>
-                            </tr>
-                        @endforeach
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="d-inline">{{__('messages.price')}}: </div>
+                                        <div class="d-inline">
+                                            {{$cart->calculatePrice($product['product_price'])}}{{$cart->getCurrencySymbol()}}
+                                        </div>
+                                        <div class="d-inline">{{__('messages.quantity')}}: </div>
+                                        <div class="d-inline col-xs-2">
+                                            <input type="hidden" value="{{$product['product_id']}}" name="product_ids[]">
+                                            <input type="hidden" value="{{$product['product_price']}}" name="product_prices[]">
+                                            <input type="number" min="0" max="999" id="{{$product['product_id']}}" name="quantity[]" class="text-center input-product-quantity-cart form-control" value="{{$product['product_quantity']}}">
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="d-inline">{{__('messages.total')}}: </div>
+                                        <div class="row-price-holder d-inline">
+                                            {{$cart->calculatePrice($product['product_row_price'])}}{{$cart->getCurrencySymbol()}}
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <span>
+                                            <a data-id="{{$product['product_id']}}" data-confirm="{{__('actions.really_delete?')}}" class="delete-from-cart" href="{{route('cart.delete', ['id' => $product['product_id']])}}" title="{{__('messages.remove_this_item_from_cart')}}">
+                                                <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
+                                            </a>
+                                        </span>
+                                    </div>
+                                </div>
+                                <hr>
+                            @endforeach
                         @endif
-                        </tbody>
-                    </table>
+                    </div>
                 </div>
                 <div class="col-md-4 col-sm-12 pt-3">
                     <div class="bg-light p-2">
